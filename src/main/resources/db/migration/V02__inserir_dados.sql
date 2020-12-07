1828c18d33c3d56bc87c39480b60392461fb8d26
Jairo <jaironsousa@gmail.com>
2020-12-07 08:35:12 -0300

    Ajuste busca de mes e ano

--- src/main/resources/db/migration/V02__inserir_dados.sql
+++ src/main/resources/db/migration/V02__inserir_dados.sql
@@ -2,7 +2,7 @@ insert into permissao (id, nome) values
 	(1, 'ROLE_ADMIN'), (2, 'ROLE_USER');
 	
 insert into usuario (id,codigo, nome,sobrenome, username, password, enabled, data_cadastro) values 
-	(1,'001', 'ACQUA','Default', 'acqua', '$2a$10$6seaJ.I/0iVkGaA5rl1FmuQCRRKx/UuFLEOsxVPMeKv2q3gXapufm', true, '2020-08-24')
+	(1,'001', 'ACQUA','Default', 'acqua', '$2a$10$g/yzMfTQhh0EuGWKPqpaB.0bAl0MM8ImOt/GqN974.sFdMrQn1Lpu', true, '2020-08-24')
 ;
 	
 insert into usuario_permissoes (usuario_id, permissao_id) values (1, 1), (1, 2);
